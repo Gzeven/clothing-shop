c3f6fec3ae8240b6aea4750f32071f60faeb8b41
Gerben van de Weerdhof <gerben1978@hotmail.com>
2020-02-26 15:47:43 +0100

    added the header component and links

--- src/App.js
+++ src/App.js
@@ -4,11 +4,13 @@ import './App.css';
 import './pages/homepage/homepage.styles.scss';
 import HomePage from './pages/homepage/homepage.component';
 import ShopPage from './pages/shop/shop.component';
+import Header from './components/header/header.component';
 
 
 function App() {
-  return (
+  return ( 
     <div >
+    <Header/>
     <Switch>
     <Route exact path='/' component={HomePage}/>
     <Route exact path='/shop' component={ShopPage}/>
