a549123c3c4322ccd7f1bfc934f27d723bedad05
Gerben van de Weerdhof <gerben1978@hotmail.com>
2020-02-26 15:27:14 +0100

    update, added shop page, collection preview component and collectio item component

--- src/App.js
+++ src/App.js
@@ -1,22 +1,17 @@
 import React from 'react';
-import './App.css';
-import HomePage from './pages/homepage/homepage.component';
-import MenuItem from './components/menu-item/menu-item.component'
-import './pages/homepage/homepage.styles.scss';
 import {Switch, Route} from 'react-router-dom'
+import './App.css';
+import './pages/homepage/homepage.styles.scss';
+import HomePage from './pages/homepage/homepage.component';
+import ShopPage from './pages/shop/shop.component';
 
-const HatsPage = () => (
-  <div>
-  <h1>Hats Page</h1>
-  </div>
-)
 
 function App() {
   return (
     <div >
     <Switch>
     <Route exact path='/' component={HomePage}/>
-    <Route exact path='/hats' component={HatsPage}/>
+    <Route exact path='/shop' component={ShopPage}/>
     </Switch>
     </div>
   );
